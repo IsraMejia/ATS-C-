4cd5654884b6aaa32ed6b629d528b7fa4600c8e9
israel mejia <ihmaisrael@gmail.com>
2020-11-04 17:02:36 -0600

    Inicie el curso de Alejandro tavuada :'D, si puedes imaginarlo, puedes programarlo

---
+++ 1-HolaMundo.cpp
@@ -0,0 +1,11 @@
+//Primer programa en C++
+#include <iostream> 
+
+using namespace std;
+
+int main () {
+	 
+	cout<< "Hola mis queridos internautas xD" <<endl;
+	return 0 ;
+	
+}
---
+++ 2-TiposDatos.cpp
@@ -0,0 +1,18 @@
+//Tipo de datos
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int numero  = 15;
+	float flotante = 9.999;
+	double decimalesplus = 3.141516;
+	char letra = 'x';
+	
+	cout << numero <<endl;
+	cout << flotante << endl;
+	cout << decimalesplus << endl;
+	cout << letra << endl;
+	
+	return 0;
+}
